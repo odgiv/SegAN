24b35ea3e1d65ee8cd97845c5c0329c92161b968
odgiiv <odgiiv.kh@gmail.com>
2019-06-25 13:22:59 +0200

    evalution code

---
+++ evaluate.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+import argparse
+import os
+import numpy as np
+from PIL import Image
+import torch
+from torch.utils import data
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+from torch import nn
+import torchvision.utils as vutils
+from torch.autograd import Variable
+import torch.nn.functional as F
+from net import NetS, NetC
+from LoadData import loader, Dataset_test
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--outpath", default="./test_outputs")
+    parser.add_argument('--batchSize', type=int, default=1)
+    parser.add_argument('--ngpu', type=int, default=1)
+    parser.add_argument('--weight_path', type=str)
+    opt = parser.parse_args()
+
+    try:
+        os.makedirs(opt.outpath)
+    except OSError:
+        pass
+    
+    dataloader_test = loader(Dataset_test('./'), opt.batchSize)
+
+    NetS = NetS(ngpu = opt.ngpu)
+    NetS.load_state_dict(torch.load(opt.weight_path))
+
+    NetS.eval()
+    for i, data in enumerate(dataloader_test, 1):
+        input, gt = Variable(data[0]), Variable(data[1])
+        # if cuda:
+        #     input = input.cuda()
+        #     gt = gt.cuda()
+        pred = NetS(input)
+        pred[pred < 0.5] = 0
+        pred[pred >= 0.5] = 1
+        pred = pred.type(torch.LongTensor)
+        pred_np = pred.data.cpu().numpy()
+        
+        print(pred_np.shape)
+
+        img = Image.fromarray((pred_np * 255).astype(np.uint8))
+        img.save(opt.outpath + "/" + str(i) + '.jpg') 
+
+
